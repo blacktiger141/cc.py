caa993773ced4662a5107127dfe4d35c1d8d7aae
blacktiger141 <72620290+blacktiger141@users.noreply.github.com>
2020-11-28 14:24:43 +0600

    Add files via upload

---
+++ cc.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python2
+
+import os
+import sys
+#hello world im a blacktiger141
+print ("\033[94m")
+os.system("figlet tiger")
+url = raw_input("[+] Enter name: ")
+os.system("cc %s" % (url))
